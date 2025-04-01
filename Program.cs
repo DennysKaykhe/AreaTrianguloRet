30d12c5a74c0d19ccbd8cb96fae3dbf87a37aafb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 19:50:26 -0300

    Eu amo alturas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double basee;
+double altura; 
+double area; 
+
+Console.Clear();
+
+Console.Write ("Base..: ");
+basee = Convert.ToDouble(Console.ReadLine());
+
+Console.Write ("Altura: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+area = basee * altura / 2;
+
+Console.WriteLine (@$"
+Área..: {area}");
